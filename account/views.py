f595704dff3dd699adff75378a4fa9b767b0d383
rawteech <rotichbil636@gmail.com>
2017-04-07 23:51:35 +0300

    Validate login form and authenticate user

--- account/views.py
+++ account/views.py
@@ -1,3 +1,34 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+from account.forms import LoginForm
+
+
+def user_login(request):
+	context = dict()
+
+	if request.method == 'POST':
+		form = LoginForm(request.POST)
+
+		# Validate form data
+		if form.is_valid():
+			cd = form.cleaned_data
+			user = authenticate(username=cd['username'], password=cd['password'])
+
+			if user is not None:
+				if user.is_active:
+					login(request, user)
+					return HttpResponse('Authenticated Successfully')
+
+				else:
+					return HttpResponse('Disabled account')
+
+			else:
+				return HttpResponse('Invalid Login')
+
+	else:
+		form = LoginForm()
+
+	context['form'] = form
+	return render(request, 'account/login.html', context)

ed042d3b1260d5cad2fbae4e5a7e8e4ebdfbf9b5
rawteech <rotichbil636@gmail.com>
2017-04-08 20:24:01 +0300

    Change urls to use Django's authentication framework

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,11 @@ from account import views
 
 
 urlpatterns = [
-	# post views
-	url(r'^login/$', views.user_login, name='login'),
+	# # previous login view
+	# url(r'^login/$', views.user_login, name='login'),
+	url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+	url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+	url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', 
+		name='logout_then_login'),
+	url(r'^$', views.dashboard, name='dashboard'),
 ]

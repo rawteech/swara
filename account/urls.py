865574086fc31b618bce83f73077002bd9f8c882
rawteech <rotichbil636@gmail.com>
2017-04-08 23:15:25 +0300

    Add urls for password reset

--- account/urls.py
+++ account/urls.py
@@ -16,4 +16,14 @@ urlpatterns = [
 		name='password_change'),
 	url(r'^password-change/done/$', 'django.contrib.auth.views.password_change_done', 
 		name='password_change_done'),
+
+	# urls to allow password reset
+	url(r'^password-reset/$', 'django.contrib.auth.views.password_reset', 
+		name='password_reset'),
+	url(r'^password-reset/done/$', 'django.contrib.auth.views.password_reset_done', 
+		name='password_reset_done'),
+	url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+		'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
+	url(r'^password-reset/complete/$', 'django.contrib.auth.views.password_reset_complete', 
+		name='password_reset_complete'),
 ]

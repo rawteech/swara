86227a42103f8d2c8757950bf22b197361398251
rawteech <rotichbil636@gmail.com>
2017-04-09 15:00:31 +0300

    Create view for profile editing

--- account/views.py
+++ account/views.py
@@ -2,8 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-from account.forms import LoginForm, UserRegistrationForm
+from account.forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from account.models import Profile
 
 
 # def user_login(request):
@@ -53,8 +55,37 @@ def register(request):
 			new_user.set_password(user_form.cleaned_data['password'])
 			# Now save the user object
 			new_user.save()
+			# Create the user profile
+			profile = Profile.objects.create(user=new_user)
+
 			return render(request, 'account/register_done.html', {'new_user': new_user})
 	else:
 		user_form = UserRegistrationForm()
 
 	return render(request, 'account/register.html', {'user_form': user_form})
+
+
+@login_required
+def edit(request):
+	context = dict()
+	
+	if request.method == 'POST':
+		user_form = UserEditForm(instance=request.user, data=request.POST)
+		profile_form = ProfileEditForm(
+			instance=request.user.profile, 
+			data=request.POST, 
+			files=request.FILES)
+
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+			messages.success(request, 'Profile updated successfully')
+		else:
+			messages.error(request, 'Error updating your profile')
+	else:
+		user_form = UserEditForm(instance=request.user)
+		profile_form = ProfileEditForm(instance=request.user.profile)
+
+	context['user_form'] = user_form
+	context['profile_form'] = profile_form
+	return render(request, 'account/edit.html', context)

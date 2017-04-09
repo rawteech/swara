4803a9366e7bfcf136c2515700682b3c780015fe
rawteech <rotichbil636@gmail.com>
2017-04-09 11:32:26 +0300

    Validate and render user registration form

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-from account.forms import LoginForm
+from account.forms import LoginForm, UserRegistrationForm
 
 
 # def user_login(request):
@@ -40,3 +40,21 @@ def dashboard(request):
 
 	context['section'] = 'dashboard'
 	return render(request, 'account/dashboard.html', context)
+
+
+def register(request):
+	if request.method == 'POST':
+		user_form = UserRegistrationForm(request.POST)
+
+		if user_form.is_valid():
+			# Create new user object without saving
+			new_user = user_form.save(commit=False)
+			# Set the chosen password
+			new_user.set_password(user_form.cleaned_data['password'])
+			# Now save the user object
+			new_user.save()
+			return render(request, 'account/register_done.html', {'new_user': new_user})
+	else:
+		user_form = UserRegistrationForm()
+
+	return render(request, 'account/register.html', {'user_form': user_form})

f7381d4a611e33148f44cec51893c2354d7bccb2
rawteech <rotichbil636@gmail.com>
2017-04-18 14:48:38 +0300

    Create image like view to accept Ajax

--- images/views.py
+++ images/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 from images.forms import ImageCreateForm
 from images.models import Image
 
@@ -40,3 +42,26 @@ def image_detail(request, id, slug):
 	context['section'] = 'images'
 	context['image'] = image
 	return render(request, 'images/image/detail.html', context)
+
+
+@login_required
+@require_POST
+def image_like(request):
+	image_id = request.POST.get('id')
+	action = request.POST.get('action')
+	
+	if image_id and action:
+		try:
+			image = Image.objects.get(id=image_id)			
+			
+			if action == 'like':
+				image.users_like.add(request.user)
+			else:
+				image.users_like.remove(request.user)
+			
+			return JsonResponse({'status': 'ok'})
+		
+		except:
+			pass
+	
+	return JsonResponse({'status': 'no'})

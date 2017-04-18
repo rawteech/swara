db4bcc273afaf8589ad5cc066eeabfa2113a28d3
rawteech <rotichbil636@gmail.com>
2017-04-18 14:49:26 +0300

    Include url for like button

--- images/urls.py
+++ images/urls.py
@@ -4,4 +4,5 @@ from images import views
 urlpatterns = [
 	url(r'^create/$', views.image_create, name='create'),
 	url(r'^detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.image_detail, name='detail'),
+	url(r'^like/$', views.image_like, name='like'),
 ]
